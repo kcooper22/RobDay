24c9397cf8d5bebc7a1ae7daba152e62fa4bb4a4
Kyle Cooper <kylecooper22@gmail.com>
2016-01-13 15:33:32 -0500

    added require lines to server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var express      = require('express'),
     md5          = require('md5'),
     cookieParser = require('cookie-parser'),
     request 	 = require('request'),
+    twitter		 = require('twitter'),
+    spotify		 = require('spotify')
 
 var port         = process.env.PORT || 3000;
 var app          = express();
@@ -16,3 +18,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(cookieParser());
+
+// ======
+// LISTENER
+app.listen(port);
+
+// ======
+// MODELS
+var User = require('./models/user.js');
